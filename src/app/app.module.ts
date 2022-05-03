ae663231b05d151d0e6d1f71e396927a8533685e
Andre Moura <andremoura500@gmail.com>
2022-05-03 15:13:45 -0300

    feat: Criado a page home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import AppRoutingModule from './app-routing.module';
 import AppComponent from './app.component';
+import HomeComponent from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
   ],
   imports: [
     BrowserModule,
